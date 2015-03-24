34bc1f76b738625dba4bb80bd4381eb018133c44
Thomas <thomas@hiron.me>
2015-03-24 10:13:41 +0100

    Fonction inutile commentée supprimée.

--- app/src/main/java/project/gobelins/wasabi/Wasabi.java
+++ app/src/main/java/project/gobelins/wasabi/Wasabi.java
@@ -12,8 +12,6 @@ public class Wasabi extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wasabi);
-
-//        UiChangeListener();
     }
 
     @Override
@@ -33,26 +31,4 @@ public class Wasabi extends Activity
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
-
-//    public void UiChangeListener()
-//    {
-//        final View decorView = getWindow().getDecorView();
-//        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
-//        {
-//            @Override
-//            public void onSystemUiVisibilityChange(int visibility)
-//            {
-//                if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
-//                {
-//                    decorView.setSystemUiVisibility(
-//                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-//                }
-//            }
-//        });
-//    }
 }
