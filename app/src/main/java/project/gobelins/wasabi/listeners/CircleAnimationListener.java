e65c47e7583f5eff1e7000076919d782bf10bb64
Thomas <thomas@hiron.me>
2015-04-29 14:04:10 +0200

    Suppression de la vue au clic sur le bouton fermer.

--- app/src/main/java/project/gobelins/wasabi/listeners/CircleAnimationListener.java
+++ app/src/main/java/project/gobelins/wasabi/listeners/CircleAnimationListener.java
@@ -2,11 +2,13 @@ package project.gobelins.wasabi.listeners;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 
 import io.codetail.animation.SupportAnimator;
 import io.codetail.animation.ViewAnimationUtils;
+import project.gobelins.wasabi.R;
 
 /**
  * Created by ThomasHiron on 29/04/2015.
@@ -15,13 +17,32 @@ public class CircleAnimationListener implements View.OnClickListener
 {
     private View mView;
     private View mRevealContainer;
+    private boolean mClose; /* Animation fermante */
 
     private final int REVEAL_DURATION = 300;
 
+    /**
+     * On ouvre la page
+     *
+     * @param revealContainer
+     */
     public CircleAnimationListener(View revealContainer)
     {
         mRevealContainer = revealContainer;
         mView = ((ViewGroup) mRevealContainer).getChildAt(0);
+        mClose = false;
+    }
+
+    /**
+     * On ferme la page
+     * @param revealContainer
+     * @param b
+     */
+    public CircleAnimationListener(View revealContainer, boolean b)
+    {
+        mRevealContainer = revealContainer;
+        mView = ((ViewGroup) mRevealContainer).getChildAt(0);
+        mClose = b;
     }
 
     @Override
@@ -40,8 +61,51 @@ public class CircleAnimationListener implements View.OnClickListener
         /* Le rayon final */
         float finalRadius = hypo(mView.getWidth(), mView.getHeight());
 
-        /* On démarre l'animation */
-        SupportAnimator reveal = ViewAnimationUtils.createCircularReveal(mView, cx, cy, 0, finalRadius);
+        SupportAnimator reveal;
+
+        /* On instancie l'animation */
+        if(mClose)
+            reveal = ViewAnimationUtils.createCircularReveal(mView, cx, cy, finalRadius, 0);
+        else
+            reveal = ViewAnimationUtils.createCircularReveal(mView, cx, cy, 0, finalRadius);
+
+        /* Ajout du listener sur fermer */
+        Button close = (Button) mRevealContainer.findViewById(R.id.close_fresco);
+        close.setOnClickListener(new CircleAnimationListener(mRevealContainer, true));
+
+        /* Si animation fermante, on supprime la vue lorsque l'animation est terminée */
+        if(mClose)
+        {
+            reveal.addListener(new SupportAnimator.AnimatorListener()
+            {
+                @Override
+                public void onAnimationStart()
+                {
+
+                }
+
+                @Override
+                public void onAnimationEnd()
+                {
+                    ViewGroup parent = (ViewGroup) mRevealContainer.getParent();
+                    parent.removeView(mRevealContainer);
+                }
+
+                @Override
+                public void onAnimationCancel()
+                {
+
+                }
+
+                @Override
+                public void onAnimationRepeat()
+                {
+
+                }
+            });
+        }
+
+        /* On paramètre et lance l'animation */
         reveal.setInterpolator(new AccelerateDecelerateInterpolator());
         reveal.setDuration(REVEAL_DURATION);
         reveal.start();
