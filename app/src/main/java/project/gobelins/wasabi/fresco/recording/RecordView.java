867b19cbe3c840a030bd25581d1ff19f54c9fea1
Thomas <thomas@hiron.me>
2015-05-03 17:41:31 +0200

    Ajout de la vue du bouton enregistrer.

---
+++ app/src/main/java/project/gobelins/wasabi/fresco/recording/RecordView.java
@@ -0,0 +1,22 @@
+package project.gobelins.wasabi.fresco.recording;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+/**
+ * Created by ThomasHiron on 03/05/2015.
+ */
+public class RecordView extends LinearLayout
+{
+    public RecordView(Context context)
+    {
+        super(context);
+    }
+
+    public RecordView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/project/gobelins/wasabi/fresco/views/buttons/StartRecordingButton.java
@@ -0,0 +1,28 @@
+package project.gobelins.wasabi.fresco.views.buttons;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import project.gobelins.wasabi.fresco.recording.RecordView;
+
+/**
+ * Created by ThomasHiron on 03/05/2015.
+ */
+public class StartRecordingButton extends Button
+{
+    public StartRecordingButton(Context context)
+    {
+        super(context);
+    }
+
+    public StartRecordingButton(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+}
