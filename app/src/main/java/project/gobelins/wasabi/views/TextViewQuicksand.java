bf58c3bca925bbd6804771bf15c123a97b391335
Thomas <thomas@hiron.me>
2015-05-25 20:13:22 +0200

    Ajout de la possibilité de passer un attribut pour avoir la typo Light.

--- app/src/main/java/project/gobelins/wasabi/views/TextViewQuicksand.java
+++ app/src/main/java/project/gobelins/wasabi/views/TextViewQuicksand.java
@@ -1,15 +1,20 @@
 package project.gobelins.wasabi.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
+
+import project.gobelins.wasabi.R;
 
 /**
  * Created by ThomasHiron on 21/05/2015.
  */
 public class TextViewQuicksand extends TextView
 {
+    private String mFontStyle;
+
     public TextViewQuicksand(Context context, AttributeSet attrs, int defStyle)
     {
         super(context, attrs, defStyle);
@@ -19,6 +24,27 @@ public class TextViewQuicksand extends TextView
     public TextViewQuicksand(Context context, AttributeSet attrs)
     {
         super(context, attrs);
+
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.TextViewQuicksand);
+
+        mFontStyle = "Regular";
+
+        final int N = a.getIndexCount();
+        for(int i = 0; i < N; ++i)
+        {
+            int attr = a.getIndex(i);
+            switch(attr)
+            {
+                case R.styleable.TextViewQuicksand_font_style:
+
+                    mFontStyle = a.getString(attr).toLowerCase();
+                    mFontStyle = mFontStyle.substring(0,1).toUpperCase() + mFontStyle.substring(1);
+
+                    break;
+            }
+        }
+        a.recycle();
+
         init();
     }
 
@@ -30,7 +56,7 @@ public class TextViewQuicksand extends TextView
 
     private void init()
     {
-        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "Quicksand-Regular.ttf");
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "Quicksand-" + mFontStyle + ".ttf");
         setTypeface(tf);
     }
 }
