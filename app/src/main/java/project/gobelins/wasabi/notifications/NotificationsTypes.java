164101059b302f11a620eabc4c91b9f6e54fe60b
Thomas <thomas@hiron.me>
2015-05-25 16:33:52 +0200

    Ajout de nouveaux types.

--- app/src/main/java/project/gobelins/wasabi/notifications/NotificationsTypes.java
+++ app/src/main/java/project/gobelins/wasabi/notifications/NotificationsTypes.java
@@ -14,4 +14,12 @@ public class NotificationsTypes
     public final static int SOUNDS = 5;
     public final static int MESSAGES = 6;
     public final static int PLAYLISTS = 7;
+    public final static int INCRUSTATIONS = 8;
+    public final static int IDENTIKIT = 9;
+    public final static int WEBCAM = 10;
+    public final static int WEBSITELINKS = 11;
+    public final static int FACEBOOKEVENTS = 12;
+    public final static int PLACES = 13;
+    public final static int CHALLENGES = 14;
+    public final static int FINALEVENTS = 15;
 }
