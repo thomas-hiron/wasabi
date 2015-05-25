2573a819e65f0c8329b9768051cb824d27c95cc8
Thomas <thomas@hiron.me>
2015-05-25 17:41:22 +0200

    Création de la vue pour un event Facebook.

--- app/src/main/java/project/gobelins/wasabi/notifications/AsyncNotificationInflater.java
+++ app/src/main/java/project/gobelins/wasabi/notifications/AsyncNotificationInflater.java
@@ -8,6 +8,7 @@ import android.widget.FrameLayout;
 import project.gobelins.wasabi.Wasabi;
 import project.gobelins.wasabi.entities.Notification;
 import project.gobelins.wasabi.notifications.views.ChallengeView;
+import project.gobelins.wasabi.notifications.views.FacebookView;
 import project.gobelins.wasabi.notifications.views.GPSView;
 import project.gobelins.wasabi.notifications.views.MessageView;
 import project.gobelins.wasabi.notifications.views.MyLayout;
@@ -66,7 +67,7 @@ public class AsyncNotificationInflater extends AsyncTask
             case NotificationsTypes.FACEBOOKEVENTS:
 
                 /* Création de la vue */
-                mCustomView = new MessageView(mWasabi.getApplicationContext());
+                mCustomView = new FacebookView(mWasabi.getApplicationContext());
 
                 break;
 
