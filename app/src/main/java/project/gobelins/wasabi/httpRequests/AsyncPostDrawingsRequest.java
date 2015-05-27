20e96abe5af55efe9539af979b390bd4bfe34926
Thomas <thomas@hiron.me>
2015-05-27 14:37:22 +0200

    Récupération des dessins du complice OK.

--- app/src/main/java/project/gobelins/wasabi/httpRequests/AsyncPostDrawingsRequest.java
+++ app/src/main/java/project/gobelins/wasabi/httpRequests/AsyncPostDrawingsRequest.java
@@ -1,12 +1,20 @@
 package project.gobelins.wasabi.httpRequests;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
 
 import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.Iterator;
 import java.util.List;
 
 import project.gobelins.wasabi.Wasabi;
+import project.gobelins.wasabi.sqlite.tables.Drawings;
 
 /**
  * Created by ThomasHiron on 27/04/2015.
@@ -26,5 +34,43 @@ public class AsyncPostDrawingsRequest extends AsyncPostRequests
     protected void onPostExecute(String s)
     {
         super.onPostExecute(s);
+
+        /* Json decode */
+        try
+        {
+            JSONObject jsonObject = new JSONObject(s);
+
+            /* Chaque date */
+            JSONObject pointsObject = (JSONObject) jsonObject.get("points");
+            Iterator<String> keys = pointsObject.keys();
+
+            while(keys.hasNext())
+            {
+                String currentDate = keys.next();
+                JSONArray dateArray = (JSONArray) pointsObject.get(currentDate);
+
+                /* Enregistrement de chaque traits */
+                for(int i = 0, l = dateArray.length(); i < l; ++i)
+                {
+                    JSONObject currentDrawing = (JSONObject) dateArray.get(i);
+                    String points = (String) currentDrawing.get("points");
+                    String color = (String) currentDrawing.get("color");
+
+                    ContentValues contentValues = new ContentValues(3);
+                    contentValues.put(Drawings.DRAWINGS_POINTS, points.replaceAll("\\[(.*)\\]", "$1"));
+                    contentValues.put(Drawings.DRAWINGS_DATE, currentDate);
+                    contentValues.put(Drawings.DRAWINGS_COLOR, Color.parseColor(color));
+
+                    /* Enregistrement */
+                    mContext.getContentResolver().insert(Uri.parse(Drawings.URL_DRAWINGS), contentValues);
+                }
+            }
+
+
+        }
+        catch(NullPointerException | JSONException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
