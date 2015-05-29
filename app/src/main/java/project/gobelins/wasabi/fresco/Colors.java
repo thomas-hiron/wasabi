4b4452aad0d7147665ed2ab9a39830ca9aaec8d8
Thomas <thomas@hiron.me>
2015-05-29 13:42:51 +0200

    Suppression des deux bleus.

--- app/src/main/java/project/gobelins/wasabi/fresco/Colors.java
+++ app/src/main/java/project/gobelins/wasabi/fresco/Colors.java
@@ -10,8 +10,6 @@ public class Colors
     private final String BLACK = "#1A1A1A";
     private final String RED1 = "#f26667";
     private final String RED2 = "#f69494";
-    private final String BLUE1 = "#bde9eb";
-    private final String BLUE2 = "#82c7ca";
 
     private int[] mColors;
 
@@ -25,8 +23,6 @@ public class Colors
         mColors[0] = Color.parseColor(BLACK);
         mColors[1] = Color.parseColor(RED1);
         mColors[2] = Color.parseColor(RED2);
-        mColors[3] = Color.parseColor(BLUE1);
-        mColors[4] = Color.parseColor(BLUE2);
     }
 
     /**
