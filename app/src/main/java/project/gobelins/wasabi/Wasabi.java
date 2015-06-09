1e7e86715e89af2cf275ef813f900a0ecb1f3a1d
Thomas <thomas@hiron.me>
2015-06-09 11:37:55 +0200

    Affichage de l'animation d'accueil.

--- app/src/main/java/project/gobelins/wasabi/Wasabi.java
+++ app/src/main/java/project/gobelins/wasabi/Wasabi.java
@@ -154,8 +154,8 @@ public class Wasabi extends FragmentActivity implements OnFrescoOpened, OnFresco
         mLastNotification = mNotificationsManager.getLast();
 
         /* ---- TEMPORAIRE ---- */
-        mAnimPlayed = true;
-        homeAnimationEnd();
+//        mAnimPlayed = true;
+//        homeAnimationEnd();
         /* ---- FIN TEMPORAIRE ---- */
 
         /* Dès que la taille de la vue principale change, on remet le mode immersif (fermeture du clavier) */
