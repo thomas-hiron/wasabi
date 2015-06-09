27a28600dd4c2d3ba2f79ccf23e2e935eeeae939
Thomas <thomas@hiron.me>
2015-06-09 11:21:57 +0200

    Suppression de méthodes inutiles.

--- app/src/main/java/project/gobelins/wasabi/notifications/views/FinalEventView.java
+++ app/src/main/java/project/gobelins/wasabi/notifications/views/FinalEventView.java
@@ -7,25 +7,17 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.OvershootInterpolator;
 import android.view.animation.ScaleAnimation;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import project.gobelins.wasabi.R;
 import project.gobelins.wasabi.Wasabi;
-import project.gobelins.wasabi.listeners.AlphaRemoveListener;
-import project.gobelins.wasabi.views.RoundGradientChallengeView;
 
 /**
  * Created by ThomasHiron on 24/05/2015.
  */
 public class FinalEventView extends MyLayout implements View.OnClickListener
 {
-    private RoundGradientChallengeView mCounter;
     private ImageView mAcceptButton;
     private ImageView mDeclineButton;
     private TextView mAcceptText;
@@ -34,8 +26,6 @@ public class FinalEventView extends MyLayout implements View.OnClickListener
     private TextView mMeetingText;
     private View mCloseNotification;
     private boolean mFinalEventAccepted;
-    private Button mValideFullName;
-    private LinearLayout mFormView;
 
     public FinalEventView(Context context)
     {
@@ -86,9 +76,6 @@ public class FinalEventView extends MyLayout implements View.OnClickListener
         /* Ajout listener accepter */
         if(!mFinalEventAccepted)
             mAcceptButton.setOnClickListener(this);
-        else
-            mValideFullName.setOnClickListener(this);
-
     }
 
     /**
@@ -100,8 +87,6 @@ public class FinalEventView extends MyLayout implements View.OnClickListener
         /* Suppression du listener sur le bouton accepter */
         if(!mFinalEventAccepted)
             mAcceptButton.setOnClickListener(null);
-        else
-            mValideFullName.setOnClickListener(null);
     }
 
     @Override
@@ -109,8 +94,6 @@ public class FinalEventView extends MyLayout implements View.OnClickListener
     {
         if(view.getId() == R.id.accept_final_event)
             acceptButtonClicked();
-        else if(view.getId() == R.id.validate_full_name)
-            validateFullNameClicked();
     }
 
     /**
@@ -159,37 +142,5 @@ public class FinalEventView extends MyLayout implements View.OnClickListener
         /* Ajout dans les sharedPrefs */
         SharedPreferences prefs = getContext().getSharedPreferences(Wasabi.class.getSimpleName(), Context.MODE_PRIVATE);
         prefs.edit().putBoolean(Wasabi.FINAL_EVENT_ACCEPTED, true).apply();
-    }
-
-    /**
-     * Bouton valider le formulaire cliqué
-     */
-    private void validateFullNameClicked()
-    {
-        EditText fullName = (EditText) findViewById(R.id.full_name);
-        String text = fullName.getText().toString().trim();
-
-        if(text.length() != 0)
-            Toast.makeText(getContext(), "Le champ ne doit pas être vide", Toast.LENGTH_SHORT).show();
-        else
-        {
-            /* Formulaire caché via animation */
-            AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
-            alphaAnimation.setDuration(1500);
-
-            mFormView.startAnimation(alphaAnimation);
-            alphaAnimation.setAnimationListener(new AlphaRemoveListener((FrameLayout) mFormView.getParent(), mFormView));
-
-            /* Affichage de la nouvelle vue */
-            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.experience_over);
-            linearLayout.setVisibility(View.VISIBLE);
-            mCloseNotification.setVisibility(VISIBLE);
-
-            alphaAnimation = new AlphaAnimation(0, 1);
-            alphaAnimation.setDuration(1500);
-
-            linearLayout.startAnimation(alphaAnimation);
-            mCloseNotification.startAnimation(alphaAnimation);
-        }
     }
 }
